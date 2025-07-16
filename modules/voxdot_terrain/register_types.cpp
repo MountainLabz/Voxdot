239905b4fbf5038432fd81e52656ab36993fe6c5
MountainLabs <112782466+MountainLabz@users.noreply.github.com>
2025-07-15 22:44:33 -0500

    Pushed the actual module code.

---
+++ modules/voxdot_terrain/register_types.cpp
@@ -0,0 +1,18 @@
+#include "register_types.h"
+
+#include "core/object/class_db.h"
+#include "voxdot_terrain.h"
+
+void initialize_voxdot_terrain_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	ClassDB::register_class<VoxdotTerrain>();
+}
+
+void uninitialize_voxdot_terrain_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	// Nothing to do here in this example.
+}
